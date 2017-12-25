b5f48a86badd1b177fa995285c88b003943d3706
Trevor Kirpaul <tkirpaul@gmail.com>
2017-12-25 16:50:08 -0500

    final commit, course complete

--- test/reading_test.js
+++ test/reading_test.js
@@ -3,13 +3,32 @@ const User = require('../src/user');
 
 describe('Reading users out of database', () => {
   // declare here for scoping
-  let joe;
+  let alex, joe, maria, zach;
 
   beforeEach((done) => {
+    alex = new User({ name: 'Alex' });
     joe = new User({ name: 'Joe' });
-    joe.save()
+    maria = new User({ name: 'Maria' });
+    zach = new User({ name: 'Zach' });
+
+    // alex.save()
+    // .then(() => joe.save())
+    // .then(() => maria.save())
+    // .then(() => zach.save())
+    // .then(() => done());
+    
+
+    Promise.all([alex.save(), joe.save(), maria.save(), zach.save()])
       .then(() => done());
+    
   });
+
+  // afterEach(() => {
+  //   alex = null;
+  //   joe = null;
+  //   maria = null;
+  //   zach = null;
+  // })
 
   it('finds all users with a name of joe', (done) => {
     User.find({ name: 'Joe' })
@@ -27,6 +46,19 @@ describe('Reading users out of database', () => {
         assert(user.name === 'Joe');
         done();
       })
+  });
+  // using skip & limit to paginate results and sort()
+  it('can skip and limit the result set', (done) => {
+    User.find({})
+    .sort({ name: 1 })
+    .skip(1)
+    .limit(2)
+    .then((users) => {
+      assert(users.length === 2);
+      assert(users[0].name === 'Joe');
+      assert(users[1].name === 'Maria');
+      done();
+    })
   });
 
 });
